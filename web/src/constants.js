a566a3677e58d340159e25e0878a7cd0504907a0
247033 Wojciech Mokwiński <mokwinskiwojciech0@gmail.com>
2024-02-18 18:29:31 +0100

    Supabase connect

--- web/src/constants.js
+++ web/src/constants.js
@@ -1,9 +1,1 @@
-// export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || 'http://localhost:8000'
-// export const SUPABASE_KEY =
-//   import.meta.env.VITE_SUPABASE_KEY ||
-//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICAgInJvbGUiOiAiYW5vbiIsCiAgICAiaXNzIjogInN1cGFiYXNlIiwKICAgICJpYXQiOiAxNjgyNTQ2NDAwLAogICAgImV4cCI6IDE4NDAzOTkyMDAKfQ.qSqW2_EvUcfYr84Y0TIciAJi_7vUPw48eMpa2HBYKDk'
-
 export const NAVBAR_HEIGHT = '88px'
-
-// export const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY
-// export const OPENAI_API_URL = import.meta.env.VITE_OPENAI_API_URL
--- web/src/pages/statystyki/index.jsx
+++ web/src/pages/statystyki/index.jsx
@@ -1,45 +1,181 @@
-import {Stack, Text, Table, Thead, Tbody, Tr, Th, Td} from '@chakra-ui/react'
+import {Stack, Text, Table, Thead, Tbody, Tr, Th, Td, Button} from '@chakra-ui/react'
+import supabase from '../../config/supabaseClient.js'
 
 const StatisticsPage = () => {
+  console.log(supabase)
+
   const championshipsData = [
-    {rank: 1, athlete: 'John Doe', total: 800, squat: 300, bench: 200, deadlift: 300},
-    {rank: 2, athlete: 'Jane Smith', total: 750, squat: 250, bench: 200, deadlift: 300},
-    {rank: 3, athlete: 'Bob Johnson', total: 700, squat: 275, bench: 175, deadlift: 250},
-    {rank: 4, athlete: 'Alice Brown', total: 680, squat: 240, bench: 180, deadlift: 260},
-    {rank: 5, athlete: 'Charlie White', total: 670, squat: 230, bench: 185, deadlift: 255},
-    {rank: 6, athlete: 'Emma Davis', total: 660, squat: 220, bench: 190, deadlift: 250},
-    {rank: 7, athlete: 'Frank Green', total: 650, squat: 210, bench: 195, deadlift: 245},
-    {rank: 8, athlete: 'Grace Taylor', total: 640, squat: 200, bench: 200, deadlift: 240},
-    {rank: 9, athlete: 'Henry Harris', total: 630, squat: 190, bench: 205, deadlift: 235},
-    {rank: 10, athlete: 'Ivy Miller', total: 620, squat: 180, bench: 210, deadlift: 230},
-    // Add more users as needed
+    {
+      zawodnik: 'Rusek Damian',
+      Waga: '80,89',
+      'Maks WL': 120,
+      'Wilks WL': 81.3595,
+      'Maks MC': 260,
+      'Wilks MC': 176.2788,
+      Total: 380,
+      Wilks: 257.6383,
+      Klub: 'Farma Lifterów Wojny',
+      Miejsce: 1,
+    },
+    {
+      zawodnik: 'Ciachera Wiktor',
+      Waga: '81,27',
+      'Maks WL': 125,
+      'Wilks WL': 84.5048,
+      'Maks MC': 230,
+      'Wilks MC': 155.4889,
+      Total: 355,
+      Wilks: 239.9937,
+      Klub: 'AWL',
+      Miejsce: 2,
+    },
+    {
+      zawodnik: 'Wiśniewski Damian',
+      Waga: '80,47',
+      'Maks WL': 130,
+      'Wilks WL': 88.4252,
+      'Maks MC': 212.5,
+      'Wilks MC': 144.5412,
+      Total: 342.5,
+      Wilks: 232.9664,
+      Klub: null,
+      Miejsce: 3,
+    },
+    {
+      zawodnik: 'Stafa Jakub',
+      Waga: '79,21',
+      'Maks WL': 117.5,
+      'Wilks WL': 80.7247,
+      'Maks MC': 220,
+      'Wilks MC': 151.144,
+      Total: 337.5,
+      Wilks: 231.8687,
+      Klub: null,
+      Miejsce: 4,
+    },
+    {
+      zawodnik: 'Bednarczyk Rafał',
+      Waga: '80,81',
+      'Maks WL': 135,
+      'Wilks WL': 91.5855,
+      'Maks MC': 200,
+      'Wilks MC': 135.6823,
+      Total: 335,
+      Wilks: 227.2678,
+      Klub: 'AZS UMED ŁÓDŹ',
+      Miejsce: 5,
+    },
+    {
+      zawodnik: 'Studziński Witold',
+      Waga: '82,61',
+      'Maks WL': 120,
+      'Wilks WL': 80.3247,
+      'Maks MC': 187.5,
+      'Wilks MC': 125.5074,
+      Total: 307.5,
+      Wilks: 205.8321,
+      Klub: 'Turbo Dzik Team',
+      Miejsce: 6,
+    },
+    {
+      zawodnik: 'Droń Jan',
+      Waga: '81,07',
+      'Maks WL': 125,
+      'Wilks WL': 84.6331,
+      'Maks MC': 180,
+      'Wilks MC': 121.8717,
+      Total: 305,
+      Wilks: 206.5048,
+      Klub: 'PROTIP TEAM',
+      Miejsce: 7,
+    },
+    {
+      zawodnik: 'Bejgrowicz Józef',
+      Waga: '82,66',
+      'Maks WL': 75,
+      'Wilks WL': 50.1848,
+      'Maks MC': 220,
+      'Wilks MC': 147.2088,
+      Total: 295,
+      Wilks: 197.3936,
+      Klub: 'KS Team Wrocław',
+      Miejsce: 8,
+    },
+    {
+      zawodnik: 'Winiarczyk Arkadiusz',
+      Waga: '79,15',
+      'Maks WL': 112.5,
+      'Wilks WL': 77.3271,
+      'Maks MC': 145,
+      'Wilks MC': 99.6661,
+      Total: 257.5,
+      Wilks: 176.9932,
+      Klub: null,
+      Miejsce: 9,
+    },
   ]
 
   return (
     <Stack h="100%">
-      <Text fontSize="xl" fontWeight="bold" mb="4">
+      <Text fontSize="4xl" fontWeight="bold" mb="4">
         Ranking
+      </Text>
+      <Text fontSize="xl" fontWeight="bold" mb="4">
+        12.12.2012, Łódź
       </Text>
       <Table variant="striped" colorScheme="blackAlpha">
         <Thead>
           <Tr>
-            <Th>Rank</Th>
-            <Th>Athlete</Th>
+            <Th>Zawodnik</Th>
+            <Th>Waga</Th>
+            <Th>Maks WL</Th>
+            <Th>Wilks WL</Th>
+            <Th>Maks MC</Th>
+            <Th>Wilks MC</Th>
             <Th>Total</Th>
-            <Th>Squat</Th>
-            <Th>Bench</Th>
-            <Th>Deadlift</Th>
+            <Th>Wilks</Th>
+            <Th>Klub</Th>
+            <Th>Miejsce</Th>
           </Tr>
         </Thead>
         <Tbody>
-          {championshipsData.map((data) => (
-            <Tr key={data.rank}>
-              <Td>{data.rank}</Td>
-              <Td>{data.athlete}</Td>
-              <Td>{data.total}</Td>
-              <Td>{data.squat}</Td>
-              <Td>{data.bench}</Td>
-              <Td>{data.deadlift}</Td>
+          {championshipsData.map((data, index) => (
+            <Tr key={index}>
+              <Td>
+                <Button
+                  as="button"
+                  height="24px"
+                  lineHeight="1.2"
+                  transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
+                  border="1px"
+                  px="12px" // Increased padding
+                  py="8px" // Added vertical padding
+                  borderRadius="6px" // Rounded corners
+                  fontSize="14px"
+                  fontWeight="semibold"
+                  bg="red" // Set background color to red
+                  borderColor="red" // Set border color to red
+                  color="white" // Set text color to white
+                  boxShadow="0px 3px 6px rgba(0, 0, 0, 0.1)" // Added box-shadow
+                  _hover={{bg: '#ff1a1a'}} // Hover background color
+                  _active={{
+                    bg: '#cc0000', // Active background color
+                    transform: 'scale(0.98)',
+                    borderColor: '#cc0000', // Active border color
+                  }}
+                >
+                  {data.zawodnik}
+                </Button>
+              </Td>
+              <Td>{data.Waga}</Td>
+              <Td>{data['Maks WL']}</Td>
+              <Td>{data['Wilks WL']}</Td>
+              <Td>{data['Maks MC']}</Td>
+              <Td>{data['Wilks MC']}</Td>
+              <Td>{data.Total}</Td>
+              <Td>{data.Wilks}</Td>
+              <Td>{data.Klub}</Td>
+              <Td>{data.Miejsce}</Td>
             </Tr>
           ))}
         </Tbody>
